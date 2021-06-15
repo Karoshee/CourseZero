e325032b87ab98e264463b2f18d97d24e9477399
Karoshee <praslovki@mail.ru>
2021-06-15 20:49:19 +0300

    update after lesson

--- CourseZero.Scripting/Program.cs
+++ CourseZero.Scripting/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 
-namespace CourseZero.Scripting
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+// Целочисленные типы
+int i = 3;
+int j = 1;
+int k = 1;
+i = 5;
+i = i + 1;
+i = i / 2;
+i = 1 + ((i * 2)/ 2^3) - 2;
+i = j = k + 1;
+j = k + 1;
+i = j;
+
+i = i + 1;
+i += 1;
+i++;
+i--;
+
+i = 1;
+i -= 1;
+j = i++ + 1;
+
+// j = 2
+// i = 2;
+
+
+
+
+
+// Вещественные типы
+double d = 0;
+float f = 0;
+decimal dec = 0;
+
+// Символ
+char ch = 'f';
+
+// Булев тип
+bool fl = true;
+
